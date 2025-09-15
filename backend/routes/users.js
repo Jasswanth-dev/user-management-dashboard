6e4da611b34cd78de77d7b90e2c235b7017cdd07
jasswanth chinna <jasswanth3@gmail.com>
2025-09-15 13:31:53 +0530

    feat: setup express server

---
+++ backend/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../database.js');
+
+// ... define other routes (GET by ID, POST, PUT, DELETE)
+
+module.exports = router;
+
